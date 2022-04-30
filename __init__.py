6d0a42b986606a59c5a46f4e13fdc68bad244fcc
Hassan El-Sheikha <71786895+hassanelsheikha@users.noreply.github.com>
2022-04-30 17:32:00 -0400

    integrate WikiFetcher with front-end

--- __init__.py
+++ __init__.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, request, flash, send_file, \
-    send_from_directory, abort
-import subprocess
-from uipath.WikipediaFetcher import start_fetch
-import os
-import selenium
+from flask import Flask, render_template, request
+from uipath.WikiFetcher.start_wikifetcher import start_wikipedia_fetch
+from sentence_tools import extract_first_sentence
+
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    topic = request.form["topic"]
     if request.method == "POST":
+        topic = request.form["topic"]
         if topic == '':  # if the user has not given an input
             return render_template("index.html", empty="True")
         else:
-            start_fetch.start_wikipedia_fetch(topic)
-    return render_template("index.html")
+            wikipedia_definition = "Wikipedia: " + extract_first_sentence(start_wikipedia_fetch(topic))
+            return render_template("index.html", wikipedia=wikipedia_definition)
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
