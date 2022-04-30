76f675af6b89cd0d4c368ae5e2864f47b3fc9e51
hassanelsheikha <hmelsheikha@gmail.com>
2022-04-30 18:22:43 -0400

    revert to previous version

--- uipath/WikiFetcher/start_wikifetcher.py
+++ uipath/WikiFetcher/start_wikifetcher.py
@@ -1,8 +1,4 @@
-from subprocess import run
-import clipboard
 import pyperclip as pc
-import pyautogui
-from time import sleep
 import os
 UiRobotPath = r'C:\\Users\Hassan\AppData\Local\Programs\UiPath\Studio\UiRobot.exe'
 ProcessPath = r'C:\\Users\\Hassan\\Documents\\GitHub\\essay_brainstormer\\uipath\\WikiFetcher\\WikiFetcher\\Main.xaml'
