9b9a6e307591f50a19a90f8a70a9129c572344b0
hassanelsheikha <hmelsheikha@gmail.com>
2022-06-26 19:19:59 -0400

    fixed creator path in file
    -Please refer to documentation on how to configure project with your own paths using the .env file

--- uipath/WikiFetcher/start_wikifetcher.py
+++ uipath/WikiFetcher/start_wikifetcher.py
@@ -4,14 +4,20 @@ import pyperclip as pc
 from sentence_tools import extract_first_sentence
 from sentence_tools import get_key_points_google
 
-# Both the UiRobotPath and ProcessPath have to be changed with respect to the user.
-UiRobotPath = r'C:\\Users\Hassan\AppData\Local\Programs\UiPath\Studio\UiRobot.exe'
-ProcessPath = r'C:\\Users\\Hassan\\Documents\\GitHub\\essay_brainstormer\\uipath\\WikiFetcher\\WikiFetcher\\Main.xaml'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Both the UiRobotPath and ProcessPath have to set in the .env file located in
+# the main directory. Please see GitHub documentation for more details.
+UiRobotPath = os.environ.get("uirobotpath")
+ProcessPath = os.environ.get("processpath")
 
 
 def start_wikipedia_fetch(topic: str) -> str:
-    """Start the WikipediaFetch process with <phrase>, and return the contents of
-    the clipboard after the process executes. """
+    """Start the WikipediaFetch process with <phrase>, and return the contents
+    of the clipboard after the process executes. """
     topic = "'" + topic + "'"
     command = f"{UiRobotPath} execute  --file {ProcessPath} --input \"{{'in_topic': {topic}}}\" "
     os.system(f'{command}')
@@ -20,11 +26,15 @@ def start_wikipedia_fetch(topic: str) -> str:
 
 def get_wikipedia_definition(topic: str) -> Optional[tuple[str, str]]:
     """
-    >>> get_wikipedia_definition('Toronto')
+    Return a tuple containing the Wikipedia definition of <topic>, as well as a
+    string containing headlines pertaining to <topic>.
+
+    If no definition exists for <topic> on Wikipedia, return None.
     """
     first_paragraph = start_wikipedia_fetch(topic)
     print(first_paragraph)
-    definition = f'Wikipedia defines {topic} as: ' + '"' + extract_first_sentence(first_paragraph) + '"'
+    definition = f'Wikipedia defines {topic} as: ' + '"' + \
+                 extract_first_sentence(first_paragraph) + '"'
     if 'The page' in definition and 'does not exist.' in definition:
         return None
     elif '{' in definition or '}' in definition:
